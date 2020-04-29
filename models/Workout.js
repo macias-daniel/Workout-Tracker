f915bb3f8c0632c1733f26910569f072e5b895d7
PinkFished <d.macias.apps@gmail.com>
2020-04-29 16:28:48 -0700

    Add workout model

---
+++ models/Workout.js
@@ -0,0 +1,48 @@
+// import mongoose library
+const mongoose = require("mongoose");
+
+const WorkoutSchema = new mongoose.Schema({
+
+  day: {
+    type: Date,
+    required: true
+  },
+
+  exercises:
+    [
+      {
+        type: {
+          type: String,
+          required: true
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: true
+        },
+        duration: {
+          type: Number,
+          required: true,
+        },
+        //Cardio
+        distance: {
+          type: Number
+        },
+        //Resistance
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        }
+      }
+    ]
+
+})
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
