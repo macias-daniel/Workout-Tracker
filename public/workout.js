f1a24f35d94dbd02015f5cf9ca56e80b6544e989
PinkFished <d.macias.apps@gmail.com>
2020-04-30 18:14:20 -0700

    Removed console.logs()

--- public/workout.js
+++ public/workout.js
@@ -2,14 +2,13 @@
 
 async function initWorkout() {
   const lastWorkout = await API.getLastWorkout();
-  console.log(lastWorkout)
   console.log("Last workout:", lastWorkout);
   if (lastWorkout) {
     document
       .querySelector("a[href='/exercise?']")
       .setAttribute("href", `/exercise?id=${lastWorkout._id}`);
 
-    console.log(lastWorkout)
+
 
     const workoutSummary = {
       date: formatDate(lastWorkout.day),
@@ -17,7 +16,6 @@ async function initWorkout() {
       numExercises: lastWorkout.exercises.length,
       ...tallyExercises(lastWorkout.exercises)
     };
-    console.log(workoutSummary)
     renderWorkoutSummary(workoutSummary);
   } else {
     renderNoWorkoutText();
